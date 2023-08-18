574185a69341d4d14b84eddbd539d060773fec03
trunalt <trunaltamore@gmail.com>
2023-08-18 17:00:27 +0530

    adminpanel

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,10 @@
+
+<div class="flex items-center justify-center border-b-4 border-blue-500">
+    <!-- Logo -->
+    <div class="shrink-0 flex items-center py-2 bg-cyan-600">
+        <a href="">
+            <img src="images/logo/run-signature-logo.png" width="60">
+        </a>
+    </div>
+    <div class="px-4 py-4 text-2xl font-extrabold text-sky-700"> Brooks Admin Panel </div>
+</div>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,31 @@
+<!-- sidebar -->
+<div class="hidden sidebar h-full px-3 py-4 md:block w-64 left-0 z-50 h-screen bg-white">
+  <ul class="space-y-2 font-medium">
+     <li>
+        <a href="{{ route('dashboard') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+           <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+              <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+           </svg>
+           <span class="ml-3">Dashboard</span>
+        </a>
+        
+     </li>
+
+      <!-- <li>
+         <a href="{{ url('members') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+               <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
+            </svg>
+            
+            <span class="ml-3">Members</span>
+         </a>
+         
+      </li> -->
+
+      
+
+  </ul>
+</div>
+
+<!-- sidebar -->
